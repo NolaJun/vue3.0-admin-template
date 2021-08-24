d4af032af613ff18803903d8769374be455f560c
邝华 <kuanghua@ruixiude.com>
2021-08-24 18:37:10 +0800

    js to ts version

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,10 +1,12 @@
 import { asyncRoutes, constantRoutes } from '@/router'
 
+
 const state = {
   isSettingPermission: false,//是否已经设置了权限
   routes: [], //将过滤后的异步路由和静态路由集合
   addRoutes: [],//过滤后的异步路由
 }
+
 
 const mutations = {
   M_routes: (state, routes) => {
